6585a7371259b78b97cda6f8d94ebaa7bee4f124
DFGTX360 <carlos807.cf@gmail.com>
2024-06-20 12:32:08 -0300

    implementado documentação swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API swagger')
+    .setDescription('Usando documentação Swagger')
+    .setVersion('0.0.1')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
