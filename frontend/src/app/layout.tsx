a36dbd5e8462d153be42c9d68ff6fc739a39f207
Levi Santos <64612246+Carlos-Levi@users.noreply.github.com>
2024-07-08 13:20:38 +0000

    Add pagina-diversidade,(+ novos components e imgs)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
