3f0356771e88db776f2e20d3277cda17d7a14bd5
Levi Santos <64612246+Carlos-Levi@users.noreply.github.com>
2024-07-17 00:33:13 +0000

    Ajustes CardSection

--- frontend/src/components/CardSection/Card/index.tsx
+++ frontend/src/components/CardSection/Card/index.tsx
@@ -1,14 +1,17 @@
+import styles from "./styles.module.css";
+import Image from 'next/image';
+import viewMatchesImg from '../../../../public/images/view-matches.png';
+
 interface CardProps {
   title: string;
   content: string;
 }
 
-import styles from "./styles.module.css";
-
 const Card = ({ title, content }: CardProps) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardHeader}>
+        <Image src={viewMatchesImg} className={styles.cardIcon} alt="Ícone do Card" />
         <h3>{title}</h3>
       </div>
       <div className={styles.cardBody}>
