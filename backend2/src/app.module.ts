f9b15de1e5e8b540b2a9db0583b5864ec67d51d9
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-07-31 14:27:38 -0300

    documentation api backend2 and update readme

--- backend2/src/app.module.ts
+++ backend2/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-
 import { ProjectModule } from './project/project.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmConfigModule } from './configs/typeOrmConfig.module';
--- backend2/src/main.ts
+++ backend2/src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
 import { AllExceptionsFilter } from './common/filters/http-expections';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  Date.prototype.toJSON = () => {
+  Date.prototype.toJSON = function () {
     return momentTimezone(this)
       .tz('America/Sao_Paulo')
       .format('DD-MM-YYYY HH:mm:ss');
@@ -15,6 +16,15 @@ async function bootstrap() {
 
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation Backend2')
+    .setDescription('This API provides endpoints for managing projects, teams, and users. It is designed to handle CRUD (Create, Read, Update, Delete) operations efficiently.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
