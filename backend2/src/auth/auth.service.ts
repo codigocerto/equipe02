57e29a0c73ee69cb15a716bba2f1857aa7a0e2fd
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-08-04 13:43:52 -0300

    authConfigs

--- backend2/src/auth/auth.service.ts
+++ backend2/src/auth/auth.service.ts
@@ -10,17 +10,17 @@ export class AuthService {
   constructor( private jwtService: JwtService) {}
 
   // Gera um token JWT e retorna um objeto JSON contendo o token
-  async generateJwtToken(authPayload: AuthPayloadDto): Promise<{ token: string }> {
-    
+  async generateJwtToken(authPayload: AuthPayloadDto): Promise<{ token: string ,email:string}> {
+
     const payload = { email: authPayload.email, password: authPayload.password };
     
     // Gera o token usando o JwtService
     const token = this.jwtService.sign(payload);
 
     // Em vez de retornar apenas o token, retornamos um objeto JSON
-    return { token };
+    return { token, email: authPayload.email };
     
-   return 
+  
   }
 }
 
